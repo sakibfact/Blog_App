57c87b6eeb65397463b21d7bf24a63c4bb711690
sakib <sakibtech98@gmail.com>
2024-01-22 19:14:41 +0530

    Changes remove

--- src/main/java/com/blog/BloggingApplication.java
+++ src/main/java/com/blog/BloggingApplication.java
@@ -12,13 +12,7 @@ public class BloggingApplication {
 		SpringApplication.run(BloggingApplication.class, args);
 
 		System.out.println("Hi this is blogging application");
-		
-		System.out.println("hhhhhhhhhhhhhhh");
-		System.out.println("byyyyyyyyyyyy");
-		System.out.println("hello");
-		System.out.println("kiikkgkgkkkg");
-		
-		
+
 	}
 
 	@Bean
@@ -26,9 +20,9 @@ public class BloggingApplication {
 		return new ModelMapper();
 	}
 
-	
 	public void display() {
-		
+
 		System.out.println("HI i am  here ");
+
 	}
 }
